27541f8c98da10cf8bf3ecc149078e51aa8ba046
AmarokWhite <pribytkovsergey7@gmail.com>
2024-12-20 10:03:53 +0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,14 @@
+class Employee:
+    name = None
+    salary = None
+    
+employee1 = Employee()
+employee2 = Employee()
+employee1.name = 'Carl'
+employee1.salary = '300'
+employee2.name = 'Peter'
+employee2.salary = '500'
+print(employee1.name)
+print(employee1.salary)
+print(employee2.name)
+print(employee2.salary)
