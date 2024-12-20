5f2746fa25c964505bbe1aac89a77f62cc103a0c
AmarokWhite <pribytkovsergey7@gmail.com>
2024-12-20 10:08:56 +0300

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,12 @@
+class Employee:
+    def show(self):
+        return 'jsOpBsCM'
+    name = None
+    salary = None
+    
+employee1 = Employee()
+employee1.name = 'Carl'
+employee1.salary = '300'
+print(employee1.name)
+print(employee1.salary)
+print(employee1.show())
