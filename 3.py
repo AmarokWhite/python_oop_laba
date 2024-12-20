5d1b9f66cd8473d904732f2d7be4a7a4f6365bd3
AmarokWhite <pribytkovsergey7@gmail.com>
2024-12-20 09:58:16 +0300

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,12 @@
+class Employee:
+    name = None
+    age = None
+    salary = None
+    
+employee = Employee()
+employee.name = 'John'
+employee.age = '25'
+employee.salary = '5000$'
+print(employee.name)
+print(employee.age)
+print(employee.salary)
